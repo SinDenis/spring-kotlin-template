48556ce25fdab8669cb689a789f11294334ee6e2
d.sin <d.sin@tinkoff.ru>
2023-05-01 11:31:16 +0300

    Add new rest api method /api/stat

--- src/main/kotlin/com/example/springkotlintemplate/HelloController.kt
+++ src/main/kotlin/com/example/springkotlintemplate/HelloController.kt
@@ -3,12 +3,18 @@ package com.example.springkotlintemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-    
+import kotlin.random.Random
+
 @RestController
 @RequestMapping("/api")
 class HelloController {
 
     @GetMapping("/hello")
     fun hello() = "Hello"
+
+    @GetMapping("/stat")
+    fun getStat() = (1..5)
+        .map { Random.nextInt() }
+        .toList()
 
 }
